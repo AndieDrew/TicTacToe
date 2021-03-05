e6c645804561be79df61b393324b4072dfb6c2d1
AndieDrew <andrewcarlinbusiness@gmail.com>
2021-03-04 20:28:38 -0500

    Add game, player class & main js files

---
+++ src/player.js
@@ -0,0 +1,18 @@
+class Player {
+  constructor() {
+    this.id = ;
+    this.token = ;
+    this. wins = 0; //or []
+  }
+  saveWinsToStorage() {
+
+  }
+
+  retrieveWinsFromStorage() {
+
+  }
+
+}
+
+
+module.exports = Player;
